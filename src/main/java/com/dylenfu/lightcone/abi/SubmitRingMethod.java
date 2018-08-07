a5ab100889af71b57e4b3e159341d76869ecafc1
dylen <dylenfu@126.com>
2018-08-07 22:12:11 +0800

    convert submitRing params

--- src/main/java/com/dylenfu/lightcone/abi/SubmitRingMethod.java
+++ src/main/java/com/dylenfu/lightcone/abi/SubmitRingMethod.java
@@ -115,6 +115,12 @@ public class SubmitRingMethod {
         }
 
         addressList = new ArrayList<byte[]>();
+        uintArgsList = new ArrayList<BigInteger[]>();
+        uint8ArgsList = new ArrayList<BigInteger[]>();
+        buyNoMoreThanBList = new boolean[2];
+        vList = new ArrayList<BigInteger>();
+        rList = new ArrayList<byte[]>();
+        sList = new ArrayList<byte[]>();
     }
 
     private void afterUnpack() throws Exception {
@@ -134,6 +140,51 @@ public class SubmitRingMethod {
             }
         }
 
-        //Object[] objects = (Object[])list.get(0);
+        for (Object arr: (Object[]) list.get(1)) {
+            BigInteger[] numbers = new BigInteger[6];
+            Object[] sublist = (Object[]) arr;
+            for (int i = 0; i < sublist.length; i++) {
+                numbers[i] = (BigInteger) sublist[i];
+                logger.debug("submitRing uintArg " + numbers[i].toString());
+            }
+            uintArgsList.add(numbers);
+        }
+
+        for (Object arr: (Object[]) list.get(2)) {
+            Object[] sublist = (Object[]) arr;
+            BigInteger[] numbers = {(BigInteger) sublist[0]};
+            logger.debug("submitRing uint8Arg " + numbers[0]);
+            uint8ArgsList.add(numbers);
+        }
+
+        Object[] buyNoMoreThanBConvertList = (Object[]) list.get(3);
+        for (int i = 0; i < buyNoMoreThanBConvertList.length; i++) {
+            buyNoMoreThanBList[i] = (boolean) buyNoMoreThanBConvertList[i];
+            logger.debug("submitRing buyNoMoreThanB " + buyNoMoreThanBList[i]);
+        }
+
+        Object[] vConvertList = (Object[]) list.get(4);
+        for (int i = 0; i < vConvertList.length; i++) {
+            vList.add((BigInteger) vConvertList[i]);
+            logger.debug("submitRing v " + vList.get(i).toString());
+        }
+
+        Object[] rConvertList = (Object[]) list.get(5);
+        for (int i = 0; i < rConvertList.length; i++) {
+            rList.add((byte[]) rConvertList[i]);
+            logger.debug("submitRing r " + Hex.toHexString(rList.get(i)));
+        }
+
+        Object[] sConvertList = (Object[]) list.get(6);
+        for (int i = 0; i < sConvertList.length; i++) {
+            sList.add((byte[]) sConvertList[i]);
+            logger.debug("submitRing s " + Hex.toHexString(sList.get(i)));
+        }
+
+        feeRecipient = (byte[]) list.get(7);
+        logger.debug("submitRing feeRecipient " + Hex.toHexString(feeRecipient));
+
+        feeSelections = (BigInteger) list.get(8);
+        logger.debug("submitRing feeSelections " + feeSelections.toString());
     }
 }
